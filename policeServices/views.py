70bebddcaf20d69e7c4ac358a43a6ca9fa58b773
stoicism guy <pyzan55@gmail.com>
2024-02-07 20:05:48 +0500

    1

--- policeServices/views.py
+++ policeServices/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
@@ -7,4 +7,4 @@ from django.contrib.auth.decorators import login_required
 
 # @login_required(login_url='/sex/')
 def main_view(request):
-    return HttpResponse("asd")
+    return render(request, "")
